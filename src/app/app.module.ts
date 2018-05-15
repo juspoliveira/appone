68981bea8210308b720ac7a19cca0f417b483475
Juscelino Santos <juspoliveira@gmail.com>
2018-05-14 21:20:26 -0300

    Inicio de controle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,35 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FeedPageModule } from '../pages/feed/feed.module';
+import { FeedPage } from '../pages/feed/feed';
+import { IntroPageModule } from '../pages/intro/intro.module';
+import { IntroPage } from '../pages/intro/intro';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FeedPageModule,
+    IntroPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
+    MyApp,    
     HomePage,
-    TabsPage
+    TabsPage,
+    FeedPage,
+    IntroPage
   ],
   providers: [
     StatusBar,
